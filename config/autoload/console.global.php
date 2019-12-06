b91626b1c5ea0a5c26f90098fd79585d4c101e11
Alex <alex@apidemia.com>
2019-12-06 16:44:03 +0200

    Fixed an issue caused by callback functions in the console configs. (Callbacks cannot be cached => they caused an error when disabling development mode.

--- config/autoload/console.global.php
+++ config/autoload/console.global.php
@@ -1,13 +1,7 @@
 <?php
 
-use Api\App\Common\Message;
 use Api\Console\User\Handler\ListUsersHandler;
 use Api\User\Entity\UserEntity;
-use Zend\Filter\Callback;
-use Zend\Filter\StringTrim;
-use Zend\Filter\StripTags;
-use Zend\Validator\Digits;
-use Zend\Validator\InArray;
 
 /**
  * Documentation: https://github.com/zfcampus/zf-console
@@ -29,36 +23,6 @@ return [
                     'deleted' => '(Optional) Filter users by deletion status (true, false)'
                 ],
                 'defaults' => ['page' => 1, 'search' => null, 'status' => null, 'deleted' => null],
-                'filters' => [
-                    'page' => new Callback(function ($value) {
-                        $validator = new Digits();
-                        if ($validator->isValid($value)) {
-                            return (int)$value;
-                        }
-                        throw new Exception(sprintf(Message::INVALID_VALUE, 'page'));
-                    }),
-                    'search' => new Callback(function ($value) {
-                        $value = (new StringTrim())->filter($value);
-                        $value = (new StripTags())->filter($value);
-                        return $value;
-                    }),
-                    'status' => new Callback(function ($value) {
-                        $validator = new InArray();
-                        $validator->setHaystack(UserEntity::STATUSES);
-                        if ($validator->isValid($value)) {
-                            return $value;
-                        }
-                        throw new Exception(sprintf(Message::INVALID_VALUE, 'status'));
-                    }),
-                    'deleted' => new Callback(function ($value) {
-                        $validator = new InArray();
-                        $validator->setHaystack(['true', 'false']);
-                        if ($validator->isValid($value)) {
-                            return $value;
-                        }
-                        throw new Exception(sprintf(Message::INVALID_VALUE, 'deleted'));
-                    })
-                ],
                 'handler' => ListUsersHandler::class
             ]
         ]
