4e259b71acc4453f8230df9f7857e04342312711
MarioRadu <magda_marior@yahoo.com>
2024-05-02 16:29:56 +0300

    pre run hook test

--- .laminas-ci/pre-run.sh
+++ .laminas-ci/pre-run.sh
@@ -3,10 +3,10 @@ COMMAND=$(echo "${JOB}" | jq -r '.command')
 
 echo "Running $COMMAND"
 
-pecl install sqlite
-pecl install pdo_sqlite
+#pecl install sqlite
+pecl install php8.2-sqlite3
 
-php -i | grep sqlite
+#php -i | grep sqlite
 
 if [[ ${COMMAND} =~ phpunit ]];then
   mv config/autoload/local.php.dist config/autoload/local.php
