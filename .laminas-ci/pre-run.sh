61e6d17e4facb1c7e07f792379ad4c8532499f96
MarioRadu <magda_marior@yahoo.com>
2024-05-02 17:14:02 +0300

    pre run script fix

--- .laminas-ci/pre-run.sh
+++ .laminas-ci/pre-run.sh
@@ -2,16 +2,14 @@ JOB=$3
 PHP_VERSION=$4
 COMMAND=$(echo "${JOB}" | jq -r '.command')
 
-echo "Running $COMMAND @@@@@@@@@@@@@@@"
-
-#apt-get install php8.2-sqlite3
-#apt-get install php8.3-sqlite3
+echo "Running $COMMAND"
 
 if [[ ${COMMAND} =~ phpunit ]];then
 
-  apt-get install php${PHP_VERSION}-sqlite3
+  apt-get install php"${PHP_VERSION}"-sqlite3
 
   cp config/autoload/local.php.dist config/autoload/local.php
   cp config/autoload/mail.local.php.dist config/autoload/mail.local.php
   cp config/autoload/local.test.php.dist config/autoload/local.test.php
+
 fi
