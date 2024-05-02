bd49d4b2dabe957f7cd426e0c822e717dff800d1
MarioRadu <magda_marior@yahoo.com>
2024-05-02 15:12:37 +0300

    pre run hook tst

--- .laminas-ci/pre-run.sh
+++ .laminas-ci/pre-run.sh
@@ -1,9 +1,5 @@
 echo "$PWD"
 
-cd ../
-
-echo "$PWD"
-
 echo "$2"
 
 JOB=$3
@@ -12,9 +8,9 @@ COMMAND=$(echo "${JOB}" | jq -r '.command')
 ls -la
 
 echo "$COMMAND"
-if [[ ${COMMAND} =~ phpunit ]];then
+#if [[ ${COMMAND} =~ phpunit ]];then
     #mv config/autoload/local.php.dist config/autoload/local.php
     #mv config/autoload/mail.local.php.dist config/autoload/mail.local.php
     #mv config/autoload/local.test.php.dist config/autoload/local.test.php
 
-fi
+#fi
