158508b918eb2b35c77316e19e38fe7eed828b33
MarioRadu <magda_marior@yahoo.com>
2024-05-02 14:46:10 +0300

    pre run hook tst

--- .laminas-ci/pre-run.sh
+++ .laminas-ci/pre-run.sh
@@ -1,1 +1,5 @@
-echo "hello world!"
+cd ../
+
+mv config/autoload/local.php.dist config/autoload/local.php
+mv config/autoload/mail.local.php.dist config/autoload/mail.local.php
+mv config/autoload/local.test.php.dist config/autoload/local.test.php
