a030c6a497d4ec4e5d91606984e479b6e081ece8
MarioRadu <magda_marior@yahoo.com>
2024-05-02 16:05:51 +0300

    pre run hook test

--- .laminas-ci/pre-run.sh
+++ .laminas-ci/pre-run.sh
@@ -3,7 +3,9 @@ COMMAND=$(echo "${JOB}" | jq -r '.command')
 
 echo "Running $COMMAND"
 
-if [[ ${COMMAND} =~ phpunit || ${COMMAND} =~ psalm ]];then
+pecl install sqlite3
+
+if [[ ${COMMAND} =~ phpunit ]];then
   mv config/autoload/local.php.dist config/autoload/local.php
   mv config/autoload/mail.local.php.dist config/autoload/mail.local.php
   mv config/autoload/local.test.php.dist config/autoload/local.test.php
