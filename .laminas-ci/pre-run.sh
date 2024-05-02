a5563e968e3c9996e0a4394467c44bc2e0410b9a
MarioRadu <magda_marior@yahoo.com>
2024-05-02 17:42:48 +0300

    pre run script fix

--- .laminas-ci/pre-run.sh
+++ .laminas-ci/pre-run.sh
@@ -7,9 +7,9 @@ echo "Running $COMMAND"
 if [[ ${COMMAND} =~ phpunit ]];then
 
 #  apt-get update
-#  apt-get install php"${PHP_VERSION}"-sqlite3
-  apt-get install php8.2-sqlite3
-  apt-get install php8.3-sqlite3
+  apt-get install php"${PHP_VERSION}"-sqlite3 >= 8.2.18
+#  apt-get install php8.2-sqlite3
+#  apt-get install php8.3-sqlite3
 
   cp config/autoload/local.php.dist config/autoload/local.php
   cp config/autoload/mail.local.php.dist config/autoload/mail.local.php
