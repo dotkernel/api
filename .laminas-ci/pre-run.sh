0b6d23bfd94fa4139409edb0399376b2ef6612c3
MarioRadu <magda_marior@yahoo.com>
2024-05-02 17:10:28 +0300

    pre run hook test

--- .laminas-ci/pre-run.sh
+++ .laminas-ci/pre-run.sh
@@ -3,13 +3,15 @@ PHP_VERSION=$4
 COMMAND=$(echo "${JOB}" | jq -r '.command')
 
 echo "Running $COMMAND @@@@@@@@@@@@@@@"
-echo "PHP VERSION : ${PHP_VERSION}"
 
-apt-get install php8.2-sqlite3
-apt-get install php8.3-sqlite3
+#apt-get install php8.2-sqlite3
+#apt-get install php8.3-sqlite3
 
 if [[ ${COMMAND} =~ phpunit ]];then
-  mv config/autoload/local.php.dist config/autoload/local.php
-  mv config/autoload/mail.local.php.dist config/autoload/mail.local.php
-  mv config/autoload/local.test.php.dist config/autoload/local.test.php
+
+  apt-get install php${PHP_VERSION}-sqlite3
+
+  cp config/autoload/local.php.dist config/autoload/local.php
+  cp config/autoload/mail.local.php.dist config/autoload/mail.local.php
+  cp config/autoload/local.test.php.dist config/autoload/local.test.php
 fi
