41d7232a3bbb9889bf402522cbb289957c568f95
MarioRadu <magda_marior@yahoo.com>
2024-12-10 00:09:44 +0200

    upgraded to dotkernel/dot-mail 5.0
    Signed-off-by: MarioRadu <magda_marior@yahoo.com>

--- .laminas-ci/pre-run.sh
+++ .laminas-ci/pre-run.sh
@@ -9,6 +9,7 @@ if [[ ${COMMAND} =~ phpunit ]];then
   apt-get install php"${PHP_VERSION}"-sqlite3
 
   cp config/autoload/local.php.dist config/autoload/local.php
+  cp config/autoload/mail.local.php.dist config/autoload/mail.local.php
   cp config/autoload/local.test.php.dist config/autoload/local.test.php
 
 fi
