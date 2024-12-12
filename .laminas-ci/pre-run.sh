bd1e88dcd89efc280c4c973e51c97ac5e802b44e
MarioRadu <magda_marior@yahoo.com>
2024-12-12 11:52:29 +0200

    ignore development files on production env
    Signed-off-by: MarioRadu <magda_marior@yahoo.com>

--- .laminas-ci/pre-run.sh
+++ .laminas-ci/pre-run.sh
@@ -9,7 +9,6 @@ if [[ ${COMMAND} =~ phpunit ]];then
   apt-get install php"${PHP_VERSION}"-sqlite3
 
   cp config/autoload/local.php.dist config/autoload/local.php
-  cp config/autoload/mail.global.php.dist config/autoload/mail.global.php
   cp config/autoload/local.test.php.dist config/autoload/local.test.php
 
 fi
